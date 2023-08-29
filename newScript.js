5ed945f0b2171e20baca396c61bf8b118b65df36
Igor Borisenko <borisenko.igor2021@yandex.ru>
2023-08-29 20:40:33 +0300

    rec

---
+++ newScript.js
@@ -0,0 +1,73 @@
+// ============ Recursion ===================
+// ---factorial--
+
+// function rec(n) {
+// 	return n ? n * rec(n-1) : 1
+// }
+
+// console.log(rec(5));
+
+//  -- alternative --
+
+// function alt(n){
+// 	let res = 1
+// 	for(let i = n; i > 0; i--){
+// 		res *= i
+// 	}
+// 	return res
+// }
+
+// console.log(alt(5));
+
+// ============ Carried ======================
+
+// const car = (a, b, c) => {
+// 	return a*b*c
+// }
+
+// function carried(fn) {
+// 	return function carry(...args){
+// 		if(args.length >= fn.length){
+// 			return fn.apply(this, args)
+// 		}
+// 		// return carry.bind(this, ...args)
+// 		return function carry2(...newArgs){
+// 			return carry.apply(this, args.concat(newArgs))
+// 		}
+// 	}
+// }
+
+// const carriedMul = carried(car)
+
+// console.log(carriedMul(1)(2)(2));
+//  -- ALT ---
+// const car = (a, b, c) => {
+// 	return a*b*c
+// }
+
+// function carried(fn, ...args) {
+// 	return function f(...newArgs){
+// 		const allArgs = [...args, ...newArgs]
+// 		if(allArgs.length >= fn.length){
+// 			return fn(...allArgs)
+// 		}
+// 		else {
+// 			return carried(fn, ...allArgs)
+// 		}	
+// 	}
+// }
+
+// const carriedMul = carried(car)
+
+// console.log(carriedMul(1)(2)(2));
+
+// ================= Zamikanie ===================
+
+// function zam(a) {
+// 	return function (b){
+// 		return a*b
+// 	}
+// }
+// const a = zam(2)
+// console.log(a(23));
+
